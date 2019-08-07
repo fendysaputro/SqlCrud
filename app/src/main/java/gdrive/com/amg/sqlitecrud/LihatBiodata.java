e33f7dbf50fac81bac244ae6552f35a33b568720
Fendy Saputro <fendy@amg.co.id>
2019-08-07 20:09:30 +0700

    update

---
+++ app/src/main/java/gdrive/com/amg/sqlitecrud/LihatBiodata.java
@@ -0,0 +1,49 @@
+package gdrive.com.amg.sqlitecrud;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LihatBiodata extends AppCompatActivity {
+    protected Cursor cursor;
+    DataHelper dataHelper;
+    Button ton2;
+    TextView text1, text2, text3, text4, text5;
+
+    @Override
+    protected void onCreate(Bundle savedIntanceState) {
+        super.onCreate(savedIntanceState);
+        setContentView(R.layout.activity_lihat_biodata);
+
+        dataHelper = new DataHelper(this);
+        text1 = (TextView) findViewById(R.id.textView1);
+        text2 = (TextView) findViewById(R.id.textView2);
+        text3 = (TextView) findViewById(R.id.textView3);
+        text4 = (TextView) findViewById(R.id.textView4);
+        text5 = (TextView) findViewById(R.id.textView5);
+        SQLiteDatabase db = dataHelper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM biodata WHERE nama = '" + getIntent().getStringExtra("nama") + "'", null);
+        cursor.moveToFirst();
+        if (cursor.getCount()>0){
+            cursor.moveToPosition(0);
+            text1.setText(cursor.getString(0).toString());
+            text2.setText(cursor.getString(1).toString());
+            text3.setText(cursor.getString(2).toString());
+            text4.setText(cursor.getString(3).toString());
+            text5.setText(cursor.getString(4).toString());
+        }
+        ton2 = (Button) findViewById(R.id.button1);
+        ton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/gdrive/com/amg/sqlitecrud/MainActivity.java
@@ -0,0 +1,90 @@
+package gdrive.com.amg.sqlitecrud;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    String[] daftar;
+    ListView listView;
+    Menu menu;
+    protected Cursor cursor;
+    DataHelper dbCenter;
+    public static MainActivity ma;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn=(Button)findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg) {
+                Intent intent = new Intent(MainActivity.this, BuatBiodata.class);
+                startActivity(intent);
+            }
+        });
+
+        ma = this;
+        dbCenter = new DataHelper(this);
+        RefreshList();
+    }
+
+    public void RefreshList(){
+        SQLiteDatabase db = dbCenter.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM biodata", null);
+        daftar = new String[cursor.getCount()];
+        cursor.moveToFirst();
+        for (int cc = 0; cc < cursor.getCount(); cc++) {
+            cursor.moveToPosition(cc);
+            daftar[cc] = cursor.getString(1).toString();
+        }
+        listView = (ListView)findViewById(R.id.listView1);
+        listView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, daftar));
+        listView.setSelected(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int arg2, long arg3) {
+                final String selection = daftar[arg2];
+                final CharSequence[] dialogItem = {"Lihat Biodata", "Update Biodata", "Hapus Biodata"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Pilihan");
+                builder.setItems(dialogItem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        switch (item){
+                            case 0 :
+                                Intent intent = new Intent(getApplicationContext(), LihatBiodata.class);
+                                intent.putExtra("nama", selection);
+                                startActivity(intent);
+                                break;
+                            case 1 :
+                                Intent in = new Intent(getApplicationContext(), UpdateBiodata.class);
+                                in.putExtra("nama", selection);
+                                startActivity(in);
+                                break;
+                            case 2 :
+                                SQLiteDatabase db = dbCenter.getWritableDatabase();
+                                db.execSQL("delete from biodata where nama = '"+selection+"'");
+                                RefreshList();
+                                break;
+                        }
+                    }
+                });
+                builder.create().show();
+            }
+        });
+        ((ArrayAdapter)listView.getAdapter()).notifyDataSetInvalidated();
+    }
+}
